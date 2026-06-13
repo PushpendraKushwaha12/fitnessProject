e321a4901e8d037cb3ea5a3350315364fe0b65e2
Pushpendra-Kushwaha <pk1592098@gmail.com>
2026-06-13 15:08:05 +0530

    api gateway integrated

---
+++ gateway/src/test/java/com/fitness/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitness.gateway;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.testng.annotations.Test;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
